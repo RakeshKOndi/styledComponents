cc6a49dae91f6158e0606d9608359d2df6fc791e
Rakesh Kondi <rakesh.swamy.k@gmail.com>
2021-08-04 11:08:02 +0000

    styled solution

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+import {CustomButton} from './styledComponents'
+
+const App = () => (
+  <>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+  </>
+)
+
+export default App
